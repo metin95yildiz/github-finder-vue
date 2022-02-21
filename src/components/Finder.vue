4c427dfead69683d902df6082110e138f1382198
metin95yildiz <metin_y1995@hotmail.com>
2022-02-21 19:00:25 +0300

    Created Finder component

---
+++ src/components/Finder.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="search-box">
+        <h3>Search for Github Users</h3>
+        <p>
+            Enter a username and find out user information
+        </p>
+        <form class="search-form">
+            <input type="text" v-model="username" name="username" placeholder="Enter a Github username" />
+            <input type="submit" @click.prevent="search" value="Search" />
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Finder",
+    data() {
+        return {
+            username: ""
+        }
+    },
+    methods: {
+        search() {
+            this.$store.dispatch("searchUser", {
+                username: this.username
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
